1e524fc297c076bbc3ae07c7769980497989dfd0
Brett Seifried <seifriedbrett@gmail.com>
2021-09-30 13:20:30 -0700

    first test of test

--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,37 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { add_two } from '../calculations.js';
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+// removed to show test
+// function add_two(num){
+//     return num + 2;
+// }
+
+// test('time to test a function', (expect) => {
+//     //Arrange
+//     // Set up your arguments and expectations
+//     const expected = true;
+    
+//     //Act 
+//     // Call the function you're testing and set the result to a const
+//     const actual = true;
+
+//     //Expect
+//     // Make assertions about what is expected versus the actual result
+//     expect.equal(actual, expected);
+// });
+
+test('time add_two function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 4;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add_two(2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
