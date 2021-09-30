f8f9656cbc90ddf0f02321dd5dd662f8d9493d26
Brett Seifried <seifriedbrett@gmail.com>
2021-09-30 13:44:17 -0700

    first tests passed for adding

--- test/example.test.js
+++ test/example.test.js
@@ -1,7 +1,7 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
-import { add_two } from '../calculations.js';
+import { add_two, add } from '../calculations.js';
 
 const test = QUnit.test;
 
@@ -36,4 +36,30 @@ test('time add_two function', (expect) => {
     //Expect
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
+});
+
+test('add function', (expect) => {
+    // Arrange
+    const input1 = 3;
+    const input2 = 2;
+    const expected = 5;
+
+    // Act on Function
+    const actual = add(input1, input2);
+
+    // Assert
+    expect.equal(actual, expected);
+});
+
+test('add(4,5) function', (expect) => {
+    // Arrange
+    const input1 = 4;
+    const input2 = 5;
+    const expected = 9;
+
+    // Act on Function
+    const actual = add(input1, input2);
+
+    // Assert
+    expect.equal(actual, expected, 'should add 3 & 2');
 });
